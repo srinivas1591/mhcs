2746566d251b3eedf9f106958126926eac0280e0
srinivas1591 <devisrinivas.vasamsetti@gmail.com>
2021-11-06 13:12:32 +0530

    up-to-date

--- src/students/Suggestafriend.js
+++ src/students/Suggestafriend.js
@@ -4,6 +4,7 @@ import Navbarex from './Navbarex'
 const Suggestafriend = () => {
     return (
         <div>
+          <Navbarex/>
              <div class="container-fluid">
 
             <div class="row d-flex flex-row align-items-center">
